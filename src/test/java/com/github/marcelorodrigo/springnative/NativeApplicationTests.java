976103fe24e6853ebf9fa4472080107006735f59
Marcelo Wiebbelling <mrodrigow@gmail.com>
2022-09-30 14:53:46 +0200

    Spring 3.0.0-M5

---
+++ src/test/java/com/github/marcelorodrigo/springnative/NativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcelorodrigo.springnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
